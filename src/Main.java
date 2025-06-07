6f819c1b52db4d3b9960c6810c610915cff8f41a
Ahmet Furkan Örek <33ahmetorek33@gmail.com>
2025-06-07 11:36:41 +0300

    Sayı tahmin kodları eklendi

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,60 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String[][] letter = new String[7][4];
+        Random rand = new Random();
+        int number = rand.nextInt(100);
+        //int number = (int) (Math.random() * 100);
 
-        for (int i = 0; i < letter.length; i++) {
-            for (int j = 0; j < letter[i].length; j++) {
-                if (i == 0 || i == 3 || i == 6) {
-                    letter[i][j] = " * ";
-                } else if (j == 0 || j == 3) {
-                    letter[i][j] = " * ";
+        Scanner input = new Scanner(System.in);
+        int right = 0;
+        int selected;
+        int[] wrong = new int[5];
+        boolean isWin = false;
+        boolean isWrong = false;
+
+        System.out.println(number);
+        while (right < 5) {
+            System.out.print("Lütfen tahmininizi giriniz : ");
+            selected = input.nextInt();
+
+            if (selected < 0 || selected > 99) {
+                System.out.println("Lütfen 0-100 arasında bir değer giriniz.");
+                if (isWrong) {
+                    right++;
+                    System.out.println("Çok fazla hatalı giriş yaptınız. Kalan hak : " + (5 - right));
                 } else {
-                    letter[i][j] = "   ";
+                    isWrong = true;
+                    System.out.println("Bir daha hatalı girişinizde hakkınızdan düşülecektir.");
                 }
-                if (j == 3) {
-                    if (i == 0 || i == 3 || i == 6) {
-                        letter[i][j] = "   ";
-                    }
+                continue;
+            }
+
+            if (selected == number) {
+                System.out.println("Tebrikler, doğru tahmin ! Tahmin ettiğini sayı : " + number);
+                isWin = true;
+                break;
+            } else {
+                System.out.println("Hatalı bir sayı girdiniz !");
+                if (selected > number) {
+                    System.out.println(selected + " sayısı, gizli sayıdan büyüktür.");
+                } else {
+                    System.out.println(selected + " sayısı, gizli sayıdan küçüktür.");
                 }
+
+                wrong[right++] = selected;
+                System.out.println("Kalan hakkı : " + (5 - right));
             }
         }
 
-
-
-
-
-
-        for (String[] row : letter) {
-            for (String col : row) {
-                System.out.print(col);
+        if (!isWin) {
+            System.out.println("Kaybettiniz ! ");
+            if (!isWrong) {
+                System.out.println("Tahminleriniz : " + Arrays.toString(wrong));
             }
-            System.out.println();
         }
-
 
     }
 }
